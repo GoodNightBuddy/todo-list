f312e15096ab0216b748bca980f75099275cf802
Pavel <fun.love.nuts@gmail.com@>
2022-08-15 16:21:25 +0300

    Configure React app for deployment to GitHub Pages

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,34 @@
+import './NewTodo.css'
+import React, { useEffect, useRef } from "react"
+
+type NewTodoProps = {
+  onAddTodo: (text: string) => void;
+}
+
+
+const NewTodo: React.FC<NewTodoProps> = props => {
+
+  let textInputRef = useRef<HTMLInputElement>(null);
+
+
+
+  const toDoSubmitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    if(!textInputRef.current!.value) return;
+    const enteredText = textInputRef.current!.value;
+    props.onAddTodo(enteredText);
+    textInputRef.current!.value = '';
+  };
+  
+  return (
+    <form onSubmit={toDoSubmitHandler} className="form-control">
+      <div>
+        <label htmlFor="todo-text">Todo text</label>
+        <input type="text" id="todo-text" ref={textInputRef}/>
+      </div>
+      <button type="submit">ADD TODO</button>
+    </form>
+  );
+}
+
+export default NewTodo
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,24 @@
+import './TodoList.css';
+
+interface TodoListProps {
+  todos: {id: number, text: string}[];
+  onDeleteTodo: (id:number) => void;
+}
+
+
+const TodoList: React.FC<TodoListProps> = props => {
+  
+  
+  return (
+    <ul >
+      {props.todos.map(todo => (
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          <button onClick={props.onDeleteTodo.bind(null, todo.id)}>DELETE</button>
+        </li>)
+      )}
+    </ul>
+  )
+}
+
+export default TodoList
